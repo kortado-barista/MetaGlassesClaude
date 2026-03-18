b3274a8688aa23402a377dbff095fe629a0bde0e
Nadav Lidor <nadav@kortado.com>
2026-03-18 14:18:13 +0200

    Initial Commit

---
+++ MetaGlassesClaude/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MetaGlassesClaude
+//
+//  Created by Nadav Lidor on 18/03/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MetaGlassesClaude/MetaGlassesClaudeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaGlassesClaudeApp.swift
+//  MetaGlassesClaude
+//
+//  Created by Nadav Lidor on 18/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct MetaGlassesClaudeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
